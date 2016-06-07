a136d1d81d70d22f7573c11ae9dce656d56c32b0
jimeelicious <xatsukex@gmail.com>
2016-06-06 22:46:58 -0700

    Add feature for monitoring downstream Signal as well

--- cableMonitor.sh
+++ cableMonitor.sh
@@ -1,25 +1,50 @@
 #!/bin/bash
-# Motorola SB6141 Modem Cable Signal Monitor v0.1
 
-logfile="/mnt/j4networkshare/NETWORK SHARE/Jimmy/cableSignal.csv"
-modem='http://192.168.100.1/cmSignalData.htm'
+# Motorola SB6141 Modem Cable Signal Monitor v0.2
+# by jimeelicious
 
-fileheader() {
+# Set locations of downstream (ds) and upstream (us) logs
+ds_log="/mnt/j4networkshare/NETWORK SHARE/Jimmy/cableSignal_ds.csv"
+us_log="/mnt/j4networkshare/NETWORK SHARE/Jimmy/cableSignal_us.csv"
+modem='http://192.168.100.1/cmSignalData.htm'	# Modem Signal page
+sec=120	# Sleep for x seconds between readings
+
+
+header_ds() {
+/usr/bin/wget --output-document - -T2 --tries=1 -q $modem | /bin/sed '33!d' |
+/bin/grep -Eoh '[0-9]+' | sed -e 's/\ //' | /usr/bin/awk -vORS=, '{ print $1 }'|
+/usr/bin/ts Frequency\(Hz\),, | /bin/sed -e 's/,$/\n/' -e 's/\ //' >> "$ds_log"
+
+/usr/bin/wget --output-document - -T2 --tries=1 -q $modem | /bin/sed '32!d' |
+/bin/grep -Eoh '[0-9]+' | ts Ch | sed -e 's/\ //' | /usr/bin/awk -vORS=, '{ print $1 }'|
+/usr/bin/ts Date,Time, | /bin/sed -e 's/,$/\n/' -e 's/\ //' >> "$ds_log"
+}
+
+monitor_ds() {
+/usr/bin/wget --output-document - -T2 --tries=1 -q $modem |
+ /bin/sed -n 36,43p | /bin/grep -Eoh '[0-9]+ dBmV' |
+ /usr/bin/awk -vORS=, '{ print $1 }'|
+ /usr/bin/ts %F,%T,| /bin/sed -e 's/,$/\n/' -e 's/\ //' >> "$ds_log"
+}
+
+header_us() {
 /usr/bin/wget --output-document - -T2 --tries=1 -q $modem | /bin/sed '56!d' |
 /bin/grep -Eoh '[0-9][0-9]' | ts Ch | sed -e 's/\ //' | /usr/bin/awk -vORS=, '{ print $1 }'|
-/usr/bin/ts Date,Time, | /bin/sed -e 's/,$/\n/' -e 's/\ //' >> "$logfile"
+/usr/bin/ts Date,Time, | /bin/sed -e 's/,$/\n/' -e 's/\ //' >> "$us_log"
 }
 
-monitor() {
+monitor_us() {
 /usr/bin/wget --output-document - -T2 --tries=1 -q $modem | /bin/sed '60!d' |
 /bin/grep -Eoh '[0-9][0-9] dBmV' | /usr/bin/awk -vORS=, '{ print $1 }' | /usr/bin/ts %F,%T,|
-/bin/sed -e 's/,$/\n/' -e 's/\ //' >> "$logfile"
+/bin/sed -e 's/,$/\n/' -e 's/\ //' >> "$us_log"
 }
 
 
-fileheader
+
+header_ds; header_us
+
 while :
 do
- monitor
- sleep 120
+ monitor_ds; monitor_us
+ sleep $sec
 done
